5efa8e89b3dbd97e5a2061ef88b4c5ad05b3ee97
CTXO <rrcb@cin.ufpe.br>
2020-11-21 20:28:36 -0300

    collecting the data for analysis

--- models/classificacao.py
+++ models/classificacao.py
@@ -1,8 +1,12 @@
-import os
+import os, sys
+MODELO1 = ["yorkshire", "shih_tzu", "poodle", "outros"]
+MODELO2 = ["yorkshire", "shih_tzu", "poodle", "labrador", "pug", "outros"]
 
-pictures_missed = {}
-pictures_right = {}
+sys.stdout = open("out2.txt","w")
 def main(modelo):
+    pictures_missed = {}
+    pictures_right = {}
+    accuracy = {}
     if len(modelo) == 4:
         import three_breeds as model
     elif len(modelo) == 6:
@@ -13,39 +17,46 @@ def main(modelo):
         acertos = 0
 
         os.chdir(directory)
-
+        pictures_missed[directory] = []
+        pictures_right[directory] = []
         pictures = os.listdir(os.getcwd())
         total_pictures = len(pictures)
-        print(f"All bellow should be {directory}")
         for file in pictures:
             # try:
-            print(f"{file}")
             demo = model.classify(f"{file}")
             label = demo["class_name"]
             confidence = demo["confidence"]
             answer = directory
             if label.lower() == answer:
                 acertos += 1
-                pictures_right[directory] = (file, str(confidence) + "%")
+                pictures_right[directory].append({"file": file, "prediction": label, "confidence": str(confidence) + "%"})
             else:
-                pictures_missed[directory] = (file, label, str(confidence)+"%")
+                pictures_missed[directory].append({"file": file, "prediction": label, "confidence": str(confidence) + "%"})
 
             # except:
             #     print(f"imagem {file} não abriu")
-        print()
-        porcentagem = acertos / total_pictures
-        print(f"Porcentadem de acerto em {directory}: {porcentagem:.2f}")
+        porcentagem = acertos/total_pictures * 100
+        accuracy[directory] = str(porcentagem) + "%"
         os.chdir("../")
-        print(20*"*")
 
-    print(f"Pictures got right: {pictures_right}")
-    print(f"Pictures missed: {pictures_missed}")
+    return ({"acertos": pictures_right, "erros": pictures_missed}, accuracy)
 
 
+db = {}
+acc = {}
+db["modelo_1"], acc["modelo_1"] = main(MODELO1)
+db["modelo_2"], acc["modelo_2"] = main(MODELO2)
 
-modelo1 = ["yorkshire", "shih tzu", "poodle", "outros"]
-modelo2 = ["yorkshire", "shih tzu", "poodle", "labrador", "pug", "outros"]
 
-main(modelo2)
+# acc_values_1 = [float(v[:-1]) for v in acc["modelo_1"].values()]
+# acc_values_2 = [[float(v[:-1]) for v in acc["modelo_2"].values()]]
+
+# mean_acc_1 = sum(acc_values_1) / 3
+# mean_acc_2 = sum(acc_values_2) / 5
+
+print(f"Data Base = {db}")
+print(f"Acurracy = {acc}")
+# print(f"Accuracy of model 1: {mean_acc_1}")
+# print(f"Acuracy of model 2: {mean_acc_2}")
 
 
